eae5c715c2264dc1067874b468a93952cc5ec319
Shae Allen <S4lL3N@live.com>
2021-08-26 15:20:40 -0400

    Create MyLifeGoals.java

---
+++ AU_CS1/Module_1/Project_1/MyLifeGoals.java
@@ -0,0 +1,31 @@
+/**
+*Simple program to print my name along with my short, mid, and long term goals.
+*
+* Project 1
+* @author Shae Allen – CPSC-1210-AO1 
+* @version 5/24/21
+*/
+public class MyLifeGoals
+{
+   /**
+      * Prints name and goals.
+      * @param args Command line arguments – not used.
+      */
+   public static void main(String[] args)
+   {
+      System.out.println("Shae Allen\n");
+      
+      System.out.println("A couple short-term goals of mine would be to  "
+         + "make an A in this class, and to jump higher than fifteen" 
+         + " feet while kiteboarding.");
+         
+      System.out.println("A few medium-term goals of mine would be to become "
+         + "more proficient at programming and to graduate from Auburn." 
+         + " Along with improving my kiteboarding skills.");
+         
+      System.out.println("Some of my long-term goals would be to "
+         + "have a successful career in a computer science related field," 
+         + " and to complete a multiple day sailing trip.");
+   }
+
+}
