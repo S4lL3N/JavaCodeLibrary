ab97a34fce5175346cef1cbd9693472725f7b35a
Shae Allen <S4lL3N@live.com>
2021-08-26 15:20:55 -0400

    Add files via upload

---
+++ AU_CS1/Module_1/Project_1/JLetter.java
@@ -0,0 +1,29 @@
+/**
+* Simple program that displays the letter J as a large block letter
+* composed of the characters ‘J’, ‘A’, ‘V’, ‘A’.
+*
+* Project 1
+* @author Shae Allen – CPSC-1210-AO1 
+* @version 5/24/21
+*/
+public class JLetter
+{
+   /**
+      * Prints the letter J.
+      * @param args Command line arguments – not used.
+      */
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA  ");
+   }
+
+}
