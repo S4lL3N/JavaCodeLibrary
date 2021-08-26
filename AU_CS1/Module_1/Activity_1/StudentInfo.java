8cbefaf1c2f46acbd00c5480b1c7a6ccee8fdaa7
Shae Allen <S4lL3N@live.com>
2021-08-26 15:19:30 -0400

    Add files via upload

---
+++ AU_CS1/Module_1/Activity_1/StudentInfo.java
@@ -0,0 +1,28 @@
+/**
+* Simple program to print my name, and previous computer courses to the console.
+*
+* Activity 1 part A
+* @author Shae Allen – CPSC-1210-AO1 
+* @version 5/24/21
+*/
+public class StudentInfo
+{
+   /**
+      * Prints name and previous Computer Courses.
+      * @param args Command line arguments – not used.
+      */
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Shae Allen ");
+      System.out.println("Previous Computer Courses: ");
+      System.out.println("   Python");
+      System.out.println("   HTML & CSS"); 
+      System.out.println("   JavaScript"); 
+      System.out.println("   MySQL");
+      System.out.println("   Java 1 & 2");
+      System.out.println("   Unix/Linux");
+      System.out.println("   Assembly & Computer Org");  
+      System.out.println("   Networking");   
+   }
+
+}
