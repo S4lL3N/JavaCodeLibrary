bedd8b6b4477f34846b294904153f267c6e11436
Shae Allen <S4lL3N@live.com>
2021-09-08 20:46:57 -0400

    Add files via upload

---
+++ AU_CS2/Module_4/Activity_4/OnlineTextItem.java
@@ -0,0 +1,27 @@
+/**
+* OnlineTextItem class, creates a online text item for the inventory app.
+* the class is derived from the InventoryItem class and also calculates the
+* cost of the item.
+*     
+*
+* Activity 4 
+* @author  Shae Allen – CPSC-1220-AO1 
+* @version 9/08/21
+*/
+public abstract class OnlineTextItem extends InventoryItem {
+   /**
+      * OnlineTextItem Constructor. 
+      * @param nameIn the name of item.
+      * @param priceIn the price of item.
+      */ 
+   public OnlineTextItem(String nameIn, double priceIn) {
+      super(nameIn, priceIn);
+   }
+   /**
+      * calculates the cost of item plus shipping. 
+      * @return price the cost of item.
+      */  
+   public double calculateCost()  {
+      return price;
+   }
+}
